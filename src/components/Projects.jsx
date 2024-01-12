ec4e20927c70b440d990435fc824d6bce2c5e625
Joansitoh <joansitohprah@gmail.com>
2024-01-12 17:35:30 +0100

    Reveal animation

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { FaGithub, FaForward } from "react-icons/fa";
 import Title from "./objets/Title";
 import { motion } from "framer-motion";
+import Reveal from "./objets/Reveal";
 
 const Project = ({ title, description, technologies, sourceLink, link }) => {
   const openLink = ({ source }) => {
@@ -9,50 +10,46 @@ const Project = ({ title, description, technologies, sourceLink, link }) => {
   };
 
   return (
-    <motion.div
-      className="flex flex-col gap-2"
-      initial={{ opacity: 0, y: 50 }}
-      whileInView={{ opacity: 1, y: 0 }}
-      transition={{ duration: 1 }}
-      /* initial={{ opacity: 0, y: 50 }}
-      whileInView={{ opacity: 1, y: 0 }}
-      transition={{ duration: 1 }} */
-    >
-      <div className="bg-gray-800 bg-opacity-85 h-44 rounded flex flex-col justify-end items-center">
-        <div className="h-36 w-4/5 bg-white rounded"></div>
-      </div>
-      <div className="flex gap-3 items-center">
-        <p className="font-extrabold text-base">{title}</p>
-        <div className="h-1 w-full sidebar" />
-        <FaGithub
-          size={30}
-          className="cursor-pointer"
-          onClick={() => openLink({ source: true })}
-        />
-        <FaForward
-          size={30}
-          className="cursor-pointer"
-          onClick={() => openLink({ source: false })}
-        />
-      </div>
-      <div className="flex gap-1 items-center text-green-500 font-bold">
-        {technologies.map((technology, index) => (
-          <span key={index}>
-            <p className="text-sm inline">{technology}</p>
-            {index !== technologies.length - 1 && <span> - </span>}
-          </span>
-        ))}
-      </div>
-      <p className="font-light text-sm">
-        {description.length > 120
-          ? description.substring(0, 120) + "..."
-          : description}
+    <Reveal
+      children={
+        <motion.div className="flex flex-col gap-2">
+          <div className="bg-gray-800 bg-opacity-85 h-44 rounded flex flex-col justify-end items-center">
+            <div className="h-36 w-4/5 bg-white rounded"></div>
+          </div>
+          <div className="flex gap-3 items-center">
+            <p className="font-extrabold text-base">{title}</p>
+            <div className="h-1 w-full sidebar" />
+            <FaGithub
+              size={30}
+              className="cursor-pointer"
+              onClick={() => openLink({ source: true })}
+            />
+            <FaForward
+              size={30}
+              className="cursor-pointer"
+              onClick={() => openLink({ source: false })}
+            />
+          </div>
+          <div className="flex gap-1 items-center text-green-500 font-bold">
+            {technologies.map((technology, index) => (
+              <span key={index}>
+                <p className="text-sm inline">{technology}</p>
+                {index !== technologies.length - 1 && <span> - </span>}
+              </span>
+            ))}
+          </div>
+          <p className="font-light text-sm">
+            {description.length > 120
+              ? description.substring(0, 120) + "..."
+              : description}
 
-        <a href="#" className="ml-2 text-green-500">
-          Learn more{" "}
-        </a>
-      </p>
-    </motion.div>
+            <a href="#" className="ml-2 text-green-500">
+              Learn more{" "}
+            </a>
+          </p>
+        </motion.div>
+      }
+    />
   );
 };
 
---
+++ src/components/objets/Reveal.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useRef } from "react";
+import { motion, useInView, useAnimation } from "framer-motion";
+
+function Reveal({ children }) {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const mainControls = useAnimation();
+  const slideControls = useAnimation();
+
+  useEffect(() => {
+    if (isInView) {
+      mainControls.start("visible");
+      slideControls.start("visible");
+    }
+  }, [isInView]);
+
+  return (
+    <div ref={ref} className="relative overflow-hidden">
+      <motion.div
+        variants={{
+          hidden: { opacity: 0, y: 75 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        initial="hidden"
+        animate={mainControls}
+        transition={{ duration: 0.5, delay: 0.25 }}
+      >
+        {children}
+      </motion.div>
+      <motion.div
+        variants={{
+          hidden: { left: 0 },
+          visible: { left: "100%" },
+        }}
+        initial="hidden"
+        animate={slideControls}
+        transition={{ duration: 0.5, ease: "easeIn" }}
+        className="absolute top-4 bottom-4 left-0 right-0 bg-green-500"
+      />
+    </div>
+  );
+}
+
+export default Reveal;
